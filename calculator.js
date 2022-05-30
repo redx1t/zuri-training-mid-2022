88875b0bbb6da353d0c4eafbc1319b3cf05e1247
redx1t <68523591+redx1t@users.noreply.github.com>
2022-05-30 12:50:53 +0300

    task 1 - calculator - js

---
+++ calculator.js
@@ -0,0 +1,40 @@
+// Write a Javascript calculator that can perform: addition, subtraction, division, multiplication and modulus operations.
+
+//addition
+function add(a, b){
+  return a+b;
+}
+
+// subtraction
+function sub(a, b){return a -b;}
+
+// division
+function divide(a, b){return a/b;}
+
+//multiply
+function multiply(a, b){return a *b;}
+
+//modulus
+function modulus(a, b){return a % b;}
+
+//comments and potential improvements
+// taking two variables. for functions such as add, multiply. we can use ...list etc
+
+//test add
+console.log(add(7,3));
+
+
+//test sub
+console.log(sub(11, 1));
+
+
+//test divide
+console.log(divide(20, 2));
+
+
+//test multiply
+console.log(multiply(2,5));
+
+
+//test modulus
+console.log(modulus(21,11));
