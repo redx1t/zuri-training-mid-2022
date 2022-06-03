cb941d97fa6cfccddc137a10dd2bc5bae469ccbe
redx1t <68523591+redx1t@users.noreply.github.com>
2022-06-03 14:40:29 +0300

    guessing game - Functions/ Arrow Functions

---
+++ number_guessing_game.js
@@ -0,0 +1,39 @@
+// Create a number guessing game to generate a number between the range of 1 and 2.
+// The game should prompt users for their username (saved in cookie).
+// Set range as function parameter and prompt the player to predict the generated number between the given range
+// , at a correct guess, the game should award the player a point (also saved in cookie), and move them to stage 2
+//  by increasing the range limit value by 1, e.g range is from 1 and 3 for stage 2 and so on.
+// Connect replit account to github and save your program in github.
+
+let correct_answer = (min, max) => {
+  Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function guessing_game() {
+  let min = 1;
+  let max = 2;
+  let status = true;
+  let points = 0;
+  let username = prompt("Please enter your username", "");
+  if (username == null || username == "") {
+    return "please enter your username to play";
+  }
+  document.cookie = "username=${username}; points=${points}";
+
+  while (status) {
+    let choice = prompt("Select a number between ${min} and ${max}");
+    if (choice == correct_answer(min, max)) {
+      max++;
+      points++;
+      document.cookie = "username=${username}; points=${points}";
+      console.log(document.cookie);
+      window.alert("you win!!!");
+    } else {
+      status = false;
+      window.alert("you lose!!!");
+    }
+  }
+  return "Game Over";
+}
+
+guessing_game();
